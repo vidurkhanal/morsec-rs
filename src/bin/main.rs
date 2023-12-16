76934902db88100711c4afac1c5268213fa2404f
vidurkhanal <vkhanal@caldwell.edu>
2023-12-16 11:26:07 -0500

    feat: toml example built

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,11 +1,47 @@
-use morsec_rs::{MorsecStr, Parser};
+use morsec_rs::{many, prefix, take_both, take_left, take_right, while_parse, MorsecStr, Parser};
+
+type Property = (String, String);
+
+#[derive(Debug)]
+struct Section {
+    title: String,
+    properties: Vec<Property>,
+}
+
+fn is_whitespace(c: char) -> bool {
+    c == ' ' || c == '\t' || c == '\n'
+}
 
 fn main() {
-    let m = MorsecStr::from("hello, world".into());
-
-    let p = morsec_rs::map(morsec_rs::prefix("hello".into()), |x: String| {
-        x.to_uppercase()
-    })
-    .parse(m);
-    println!("{p:?}")
+    let file_contents = std::fs::read_to_string("src/bin/example.toml").unwrap();
+    let section_name_parser = take_right(
+        prefix("[".into()),
+        take_left(while_parse(|x: char| x != ']'), prefix("]".into())),
+    );
+    let property_parser = take_both(
+        take_left(
+            take_left(
+                take_left(
+                    take_right(
+                        while_parse(|x: char| is_whitespace(x)),
+                        while_parse(|x: char| !is_whitespace(x) && x != '='),
+                    ),
+                    while_parse(|x: char| is_whitespace(x)),
+                ),
+                prefix("=".into()),
+            ),
+            while_parse(|x: char| is_whitespace(x)),
+        ),
+        take_left(
+            while_parse(|x: char| !is_whitespace(x) && x != '='),
+            while_parse(|x: char| is_whitespace(x)),
+        ),
+    );
+    let section_parser = morsec_rs::map(
+        take_both(section_name_parser, many(property_parser)),
+        |(title, properties)| Section { title, properties },
+    );
+    let toml_parser = many(section_parser);
+    let parsed_toml = toml_parser.parse(MorsecStr::from(file_contents));
+    println!("{:?}", parsed_toml);
 }
